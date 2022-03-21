ea5b37f880fedd4f697c47768ca5ee76ef71ec4a
Brendan Abolivier <babolivier@matrix.org>
2022-03-21 17:11:09 +0000

    First cut of the module

---
+++ synapse_bind_sydent/__init__.py
@@ -0,0 +1,81 @@
+# Copyright 2022 The Matrix.org Foundation C.I.C.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+from typing import Any, Dict
+
+import attr
+from synapse.module_api import ModuleApi
+from synapse.module_api.errors import ConfigError
+
+logger = logging.getLogger(__name__)
+
+
+@attr.s(auto_attribs=True, frozen=True)
+class SydentBinderConfig:
+    sydent_host: str
+    use_https: bool = False
+
+
+class SydentBinder:
+    def __init__(self, config: SydentBinderConfig, api: ModuleApi) -> None:
+        # Keep a reference to the config and Module API
+        self._api = api
+        self._config = config
+
+        self._http_client = api.http_client
+
+        protocol = "https" if config.use_https else "http"
+        self._sydent_bind_url = (
+            f"{protocol}://{config.sydent_host}/_matrix/identity/internal/bind"
+        )
+
+    @staticmethod
+    def parse_config(config: Dict[str, Any]) -> SydentBinderConfig:
+        if "sydent_host" not in config or not isinstance(config["sydent_host"], str):
+            raise ConfigError("sydent_host needs to be a string")
+
+        return SydentBinderConfig(**config)
+
+    async def on_register(self, user_id: str) -> None:
+        """Binds the 3PID on registration."""
+        # Get the list of 3PIDs for this user.
+        threepids = await self._api.get_threepids_for_user(user_id)
+
+        for threepid in threepids:
+            body = {
+                "address": threepid["address"],
+                "medium": threepid["medium"],
+                "mxid": user_id,
+            }
+
+            # Bind the threepid
+            try:
+                await self._http_client.post_json_get_json(self._sydent_bind_url, body)
+            except Exception as e:
+                # If there was an error, the IS is likely unreachable, so don't try again.
+                logger.exception(
+                    "Failed to bind 3PID %s to identity server at %s: %s",
+                    threepid,
+                    self._sydent_bind_url,
+                    e,
+                )
+                return
+
+            # Store the association, so we can use this to unbind later.
+            await self._api.store_remote_3pid_association(
+                user_id,
+                threepid["medium"],
+                threepid["address"],
+                self._config.sydent_host,
+            )
---
+++ tests/__init__.py
@@ -0,0 +1,37 @@
+from asyncio import Future
+from typing import Any, Awaitable, Dict, List, TypeVar
+
+from mock import Mock
+from synapse.module_api import ModuleApi
+
+from synapse_bind_sydent import SydentBinder
+
+TV = TypeVar("TV")
+
+
+def make_awaitable(result: TV) -> Awaitable[TV]:
+    """
+    Makes an awaitable, suitable for mocking an `async` function.
+    This function is copied from Synapse's test code.
+    """
+    future = Future()  # type: ignore
+    future.set_result(result)
+    return future
+
+
+def create_module(
+    user_threepids: List[Dict[str, Any]], http_mock: Mock
+) -> SydentBinder:
+    # Create a mock based on the ModuleApi spec, but override some mocked functions
+    # because some capabilities are needed for running the tests.
+    module_api = Mock(spec=ModuleApi)
+    module_api.get_threepids_for_user = Mock(
+        return_value=make_awaitable(user_threepids)
+    )
+    module_api.store_remote_3pid_association = Mock(return_value=make_awaitable(None))
+    module_api.http_client = http_mock
+
+    # If necessary, give parse_config some configuration to parse.
+    config = SydentBinder.parse_config({"sydent_host": "test"})
+
+    return SydentBinder(config, module_api)
